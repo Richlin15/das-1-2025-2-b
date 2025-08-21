fa8e761a644c23ffd7130f97f39317f5d328ba64
usuariodogit <caua.richlin@univille.br>
2025-08-21 23:00:13 +0000

    observer

---
+++ designpatterns/src/main/java/designpatterns/singleton/Cliente.java
@@ -0,0 +1,15 @@
+package designpatterns.singleton;
+
+public class Cliente {
+    public static void main(String[] args) {
+        var segredo = "Palmeiras não tem mundial";
+
+        var singleton = Singleton.getInstance();
+        singleton.setSegredo(segredo);
+
+        System.out.println(
+            Singleton.getInstance().getSegredo()
+        );
+    }
+
+}
--- designpatterns/src/main/java/designpatterns/singleton/Singleton.java
+++ designpatterns/src/main/java/designpatterns/singleton/Singleton.java
@@ -1,17 +1,26 @@
-package br.univille.singleton;
+package designpatterns.singleton;
 
 public class Singleton {
     //parece com uma variável global
     private static Singleton instance;
+    private String segredo;
 
+    public String getSegredo() {
+        return segredo;
+    }
+    public void setSegredo(String segredo) {
+        this.segredo = segredo;
+    }
     private Singleton(){
-
+        System.out.println("Singleon: Construtor");
     }
     public static Singleton getInstance(){
+        System.out.println("Singleon:getInstance");
         if (instance == null) {
+            System.out.println("Singleon: NEW");
             instance = new Singleton();
         }
-        return this.instance;
+        return instance;
     }
 
 }
