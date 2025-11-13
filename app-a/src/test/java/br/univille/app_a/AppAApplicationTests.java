13bb23d2159ef915ad341e42e2ef705491823e4b
usuariodogit <caua.richlin@univille.br>
2025-11-13 23:37:44 +0000

    app-ab

--- app-a/src/test/java/br/univille/app_a/AppAApplicationTests.java
+++ app-a/src/test/java/br/univille/app_a/AppAApplicationTests.java
@@ -1,10 +1,10 @@
-package br.univille.microkernel_interface;
+package br.univille.app_a;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicrokernelInterfaceApplicationTests {
+class AppAApplicationTests {
 
 	@Test
 	void contextLoads() {
--- app-b/src/test/java/br/univille/app_b/AppBApplicationTests.java
+++ app-b/src/test/java/br/univille/app_b/AppBApplicationTests.java
@@ -1,10 +1,10 @@
-package br.univille.microkernel_interface;
+package br.univille.app_b;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicrokernelInterfaceApplicationTests {
+class AppBApplicationTests {
 
 	@Test
 	void contextLoads() {
